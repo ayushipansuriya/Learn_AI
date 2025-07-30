e46ba919cb20034a90fc8ce68d463a368800f009
ayushipansuriya <ayushi.aimaven@gmail.com>
2025-07-30 12:24:34 +0530

    Updated database config and .gitignore to ignore .idea folder

--- TaskAPI_postgres/db/database.py
+++ TaskAPI_postgres/db/database.py
@@ -10,9 +10,7 @@ database_url = os.getenv("DATABASE_URL")
 engine = create_engine(database_url)
 
 
-# PostgreSQL DB
-'''DATABASE_URL = "postgresql://postgres:12345678@localhost:5432/taskapi"
-engine = create_engine(DATABASE_URL)'''
+
 
 
 SessionLocal = sessionmaker(bind=engine)
